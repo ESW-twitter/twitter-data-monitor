cbdb4197a9b9ebc0ccf2329ba6118337acb78bc5
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-04-09 23:13:28 -0300

    mocked twitter user tests

--- tests/test_twitter_user.py
+++ tests/test_twitter_user.py
@@ -1,16 +1,24 @@
 import unittest
 from twitter.twitter import TwitterUser
+from mock import Mock
+
+def create_mock_twitter_user():
+	user = Mock()
+	user.name = 'Alex Siqueira'
+	user.id = 14147108
+	return user
 
 class TestTwitterUser(unittest.TestCase):
+
 	def test_isonline(self):
 		user = TwitterUser('alegomes')
 
 	def test_name_retrieval(self):
-		user = TwitterUser('siqueiralex')
+		user = create_mock_twitter_user()
 		self.assertEqual('Alex Siqueira', user.name)
 
 	def test_id_retrieval(self):
-		user = TwitterUser('alegomes')
+		user = create_mock_twitter_user()
 		self.assertEqual(14147108, user.id)
 
 
