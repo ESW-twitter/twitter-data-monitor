b3cd409e7ef45b955b43d9dc4a59c43d0447289c
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-01 16:59:49 -0300

    flask and alchemysql implementation

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'jdbc:mysql://localhost:3306/twitter_db'
+db = SQLAlchemy(app)
+
+class Report(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10))
+    csv_content = db.Column(db.LargeBinary)
+
+    def __repr__(self):
+        return '<Report %r>' % self.id
 
 @app.route('/')
 def hello_world():
-   return 'Hello World’
+   return 'Hello World'
 
 if __name__ == '__main__':
     app.run()
