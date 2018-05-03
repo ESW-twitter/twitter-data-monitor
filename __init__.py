f3b6b30ec5912e796355f70ebcab8c53a5b93db2
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-03 09:34:06 -0300

    __init__ file modularization

--- __init__.py
+++ __init__.py
@@ -2,9 +2,27 @@ import csv
 import os
 import json
 import datetime
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from modules.twitter_user import TwitterUser
-from modules.__init__ import Report
-from modules.__init__ import db
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:password@localhost/twitter_db"
+db = SQLAlchemy(app)
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(20))
+    csv_content = db.Column(db.LargeBinary)
+
+    def __init__(self, date, csv_content):
+        self.date = date
+        self.csv_content = csv_content
+
+    def __repr__(self):
+        return '<Report %r>' % self.id
 
 def generate_csv_report():
     csv_content = "nome;seguidores;tweets;seguindo;curtidas;hashtags;\n"
@@ -15,11 +33,21 @@ def generate_csv_report():
         if user.existence == True:
             aux = "{};{};{};{};{};\n".format(user.name, user.followers_count, user.tweets_count, user.following_count, user.likes_count)
             csv_content = csv_content + aux
-    # print(csv_content)
+
     f = Report(datetime.datetime.now().strftime('%d/%m/%Y'), csv_content.encode())
     db.session.add(f)
     db.session.commit()
 
+sched = BlockingScheduler()
+@sched.scheduled_job('interval', minutes=2)
+def generate_reports():
+    print('oi')
+    generate_csv_report()
+
+
+@app.route('/')
+def hello_world():
+   return  'Hello World'
 
 if __name__ == '__main__':
-    generate_csv_report()
+    app.run()
