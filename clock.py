9db439b86683ccbd4918483e84b782cb493695ab
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-06 15:36:54 -0300

    changing data recovering interval

--- clock.py
+++ clock.py
@@ -3,10 +3,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from __init__ import generate_csv_report
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', days=5)
 def generate_reports():
     print("Generating reports...")
     generate_csv_report()
-    print("Done.")
+    print("Done ")
 
 sched.start()
