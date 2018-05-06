5f95660eb2baff7ff839c0468034717cd3ba5b67
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-06 14:04:02 -0300

    adding redis to requirements

--- worker.py
+++ worker.py
@@ -6,7 +6,6 @@ from __init__ import generate_csv_report
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL')
-
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
