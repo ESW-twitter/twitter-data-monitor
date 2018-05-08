7c11a3973b2d486eb2c4ce3ef215cfbfa7245d6c
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-08 09:07:27 -0300

    changed presentation settings

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from __init__ import generate_csv_report
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', days=1)
 def generate_reports():
     print("Generating reports...")
     generate_csv_report()
