42c4005b1f06d747726f25e271cc934c47ef3362
siqueiralex <Lequizi@gmail.com>
2018-05-10 16:14:37 -0300

    only one thread running per time

--- clock.py
+++ clock.py
@@ -1,12 +1,12 @@
 from modules.twitter_user import TwitterUser
 from apscheduler.schedulers.blocking import BlockingScheduler
-from __init__ import generate_csv_report
+from __init__ import csv_report
 
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1440)
 def generate_reports():
     print("Generating reports...")
-    generate_csv_report()
+    csv_report()
     print("Done ")
 
 sched.start()
