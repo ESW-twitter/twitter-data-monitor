4b641feeb660170f5f4c1bb7d4a182278aa3635b
siqueiralex <Lequizi@gmail.com>
2018-05-10 15:31:54 -0300

    using multithreading again

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import csv
 import os
 import json
 from datetime import datetime, timedelta
+import threading
 from flask import Flask, make_response, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from modules.twitter_user import TwitterUser
@@ -11,11 +12,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
-os.environ['TWITTER_CAPTURING'] = "False" 
 
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(20))
+    date = db.Column(db.String(60))
     csv_content = db.Column(db.LargeBinary)
 
     def __init__(self, date, csv_content):
@@ -26,51 +26,55 @@ class Report(db.Model):
         return '<Report %r>' % self.id
 
 
-def generate_csv_report():
-    
-    csv_content = "nome;seguidores;tweets;seguindo;curtidas;retweets;favorites;hashtags;mentions\n"
-    file = open("helpers/politicians.json")
-    actors = json.load(file)
-    os.environ['TWITTER_CAPTURING'] = "True" 
+class generate_csv_report:
 
-    try:
-        reports = Report.query.all()
-        reports.sort(key=lambda x: x.id, reverse=True)
-        last_capture = reports[0].date.split(" ")[0].split("-")
-        day = int(last_capture[2])
-        month = int(last_capture[1])
-        year = int(last_capture[0])
-        day_hour = reports[0].date.split(" ")[1].split(":")
-        hour = int(day_hour[0])
-        minute= int(day_hour[1])
-    except Exception as e:
-            yesterday = datetime.utcnow() - timedelta(days=1)
-            day = yesterday.day
-            month = yesterday.month
-            year = yesterday.year
-            hour = 0
-            minute = 0
-    
-    print("Collecting information from "+str(year)+"/"+str(month)+"/"+str(day)+" "+str(hour)+":"+str(minute)+" to date")
-      
-    for row in actors:
-        user = TwitterUser(row["twitter_handle"])
-        if user.existence == True:
-            print("Retrieving information of "+ str(user.username))
-            user.retrieve_info_from(day, month, year, hour, minute)
-            aux = "{};{};{};{};{};{};{};{};{};\n".format(user.name, user.followers_count,
-            user.tweets_count, user.following_count, user.likes_count, user.retweets_count, user.favorites_count, CsvBuilder.list_to_string(user.hashtags, hashtag=True),CsvBuilder.list_to_string(user.mentions))
-            csv_content = csv_content + aux
+    def __init__(self):
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                       # Daemonize thread
+        thread.start()                   
+    def run(self):
+        
+        csv_content = "nome;seguidores;tweets;seguindo;curtidas;retweets;favorites;hashtags;mentions\n"
+        file = open("helpers/politicians.json")
+        actors = json.load(file)
 
-
-
-    name = str(datetime.utcnow())+"-from-"+str(year)+"-"+str(month)+"-"+str(day)+" "+str(hour)+":"+str(minute)        
-    f = Report(name, csv_content.encode())
-    db.session.add(f)
-    db.session.commit()
-    os.environ['TWITTER_CAPTURING'] = "True" 
+        try:
+            reports = Report.query.all()
+            reports.sort(key=lambda x: x.id, reverse=True)
+            last_capture = reports[0].date.split(" ")[0].split("-")
+            day = int(last_capture[2])
+            month = int(last_capture[1])
+            year = int(last_capture[0])
+            day_hour = reports[0].date.split(" ")[1].split(":")
+            hour = int(day_hour[0])
+            minute= int(day_hour[1])
+        except Exception as e:
+                yesterday = datetime.utcnow() - timedelta(days=1)
+                day = yesterday.day
+                month = yesterday.month
+                year = yesterday.year
+                hour = 0
+                minute = 0
+        
+        print("Collecting information from "+str(year)+"/"+str(month)+"/"+str(day)+" "+str(hour)+":"+str(minute)+" to date")
+          
+        for row in actors:
+            user = TwitterUser(row["twitter_handle"])
+            if user.existence == True:
+                print("Retrieving information of "+ str(user.username))
+                user.retrieve_info_from(day, month, year, hour, minute)
+                aux = "{};{};{};{};{};{};{};{};{};\n".format(user.name, user.followers_count,
+                user.tweets_count, user.following_count, user.likes_count, user.retweets_count, user.favorites_count, CsvBuilder.list_to_string(user.hashtags, hashtag=True),CsvBuilder.list_to_string(user.mentions))
+                csv_content = csv_content + aux
 
 
+
+        name = str(datetime.utcnow())+"-from-"+str(year)+"-"+str(month)+"-"+str(day)+" "+str(hour)+":"+str(minute)        
+        f = Report(name, csv_content.encode())
+        db.session.add(f)
+        db.session.commit()
+
+        
 @app.route('/')
 def hello_world():
     reports = Report.query.all()
@@ -81,10 +85,7 @@ def hello_world():
 
 @app.route('/performcapture')
 def perform():
-    if os.environ.get('TWITTER_CAPTURING') == "False": 
-        generate_csv_report()
-    else:
-        print("ERRO: captura em curso, aguarde o final desta captura para realizar outra.")    
+    capture = generate_csv_report()
     return redirect("/")
 
 
