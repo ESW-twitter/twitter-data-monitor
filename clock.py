b49fa2b58c30c5e1b5dec70cbe1880ccdc5daf0d
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-12 17:41:54 -0300

    fix to env var

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from __init__ import csv_report
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes= os.environ.get('CAPTURE_INTERVAL'))
+@sched.scheduled_job('interval', minutes= int(os.environ.get('CAPTURE_INTERVAL')) )
 def generate_reports():
     print("Generating reports...")
     capture = csv_report()
