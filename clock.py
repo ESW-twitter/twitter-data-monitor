9ce4ba8a5a4932c7f2d9970d602d8b9d65e43523
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-12 17:36:57 -0300

    capture interval as a environmental variable

--- clock.py
+++ clock.py
@@ -1,9 +1,10 @@
+import os
 from modules.twitter_user import TwitterUser
 from apscheduler.schedulers.blocking import BlockingScheduler
 from __init__ import csv_report
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes=1440)
+@sched.scheduled_job('interval', minutes= os.environ.get('CAPTURE_INTERVAL'))
 def generate_reports():
     print("Generating reports...")
     csv_report()
