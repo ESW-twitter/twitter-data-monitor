590e591a10de266907401686474308f8edb45c7a
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-19 14:01:52 -0300

    added get actors endpoint

--- app/api_routes.py
+++ app/api_routes.py
@@ -1,6 +1,21 @@
 from app import app
+import json
+from flask import jsonify
+
+@app.route('/api/actors')
+def api_get_actors():
+	data = {}
+	politicians = []
+	actors_count = 0
+	actors = json.load(open("helpers/politicians.json"))
+	for actor in actors:
+		if actor['twitter_handle'] != "":
+			actors_count += 1
+			politicians.append(actor['twitter_handle'])
+	data['length'] = actors_count
+	data['actors'] = politicians
+
+	return jsonify(data)
 
 
-@app.route('/api/teste_api')
-def teste_api():
-	return "TESTE"
+## { actors: [ { 'handle': 'handle1'} , { 'handle2': 'handle' }]}
