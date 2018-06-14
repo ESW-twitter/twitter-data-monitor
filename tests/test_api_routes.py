b3165b6db784376508c0865ff69b0adacf2ac800
Marcospbb <krioni.x@gmail.com>
2018-06-14 09:16:22 -0300

    increase test coverage

--- tests/test_api_routes.py
+++ tests/test_api_routes.py
@@ -6,7 +6,7 @@ import os
 
 from app import app,db
 from app.api_routes import *
-from app.models import Actor
+from app.models import Actor,TweetReport
 from modules.twitter_user import TwitterUser
 
 app = Flask(__name__)
@@ -67,6 +67,29 @@ class TestAPIRoutes():
         # assert 1 == response.get_data().decode()
         assert '400' == json.loads(response.get_data().decode())['code']
 
+    def test_api_get_actor_account_date_none_date(self):
+        user = TwitterUser('Renova_BR')
+        user = Actor(user.id, user.username, user.name)
+        with app.app_context():
+            TestAPIRoutes.setUp()
+            db.session.add(user)
+            db.session.commit()
+            response = api_get_actor_account_date('Renova_BR', None)
+            TestAPIRoutes.tearDown()
+        assert '200' == json.loads(response.get_data().decode())['code']
+
+    def test_api_get_actor_account_date_zero_reports(self):
+        user = TwitterUser('Renova_BR')
+        user = Actor(user.id, user.username, user.name)
+        with app.app_context():
+            TestAPIRoutes.setUp()
+            db.session.add(user)
+            db.session.commit()
+            response = api_get_actor_account_date('Renova_BR', '01-01-2018')
+            TestAPIRoutes.tearDown()
+        assert '500' == json.loads(response.get_data().decode())['code']
+
+
     def test_api_get_actor_account_date(self):
         csv_content = 'nome;seguidores;tweets;seguindo;curtidas;retweets;favorites;hashtags;mentions\n923257662569148417;1715;146;193;104;0;0;;;\n'
         a = ActorReport('01-01-2018', '12:00', csv_content.encode())
@@ -81,3 +104,18 @@ class TestAPIRoutes():
             TestAPIRoutes.tearDown()
 
         assert '{"12:00":{"date":"01-01-2018","followers_count":"193","following_count":"104","likes_count":"0","name":"1715","tweets_count":"0","username":"146"},"code":"200","message":["Success"]}\n' == response.get_data().decode()
+
+    def test_api_get_actor_account_date_tweets(self):
+        csv_content = 'Data;Texto;Autor(RT);Retweets;Curtidas;Hashtags;Mentions\n2018-06-08 14:25:30;@MarcioQuessada Pressionar e discutir. Essa mudanca dificilmente vai vir deles!;;0;0; ;MarcioQuessada;'
+        user = TwitterUser('Renova_BR')
+        user = Actor(user.id, user.username, user.name)
+        t = TweetReport('12-06-2018', '19:00',user.id, csv_content.encode())
+        with app.app_context():
+            TestAPIRoutes.setUp()
+            db.session.add(t)
+            db.session.add(user)
+            db.session.commit()
+            response = api_get_actor_account_date_tweets('Renova_BR', '12-06-2018')
+            TestAPIRoutes.tearDown()
+        print()
+        assert '{"code":"200","message":"Success","tweets":[{"date":"2018-06-08 14:25:30","hashtags":" ","likes":"0","mention":"MarcioQuessada","retweets":"0","rt_author":"","text":"@MarcioQuessada Pressionar e discutir. Essa mudanca dificilmente vai vir deles!"}]}\n'== response.get_data().decode()
